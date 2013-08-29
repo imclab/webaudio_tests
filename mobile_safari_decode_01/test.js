524d8e2dab543db9598eb34bdd7b0913b3f68e0d
chris_coniglio <chris@dinahmoe.com>
2013-08-29 08:19:58 -0700

    adding mobile safari test

---
+++ mobile_safari_decode_01/test.js
@@ -0,0 +1,32 @@
+(function () {
+    var context = new webkitAudioContext(),
+        fileName = "test.aac",
+        numberOfTimes = 10000,
+        ref = numberOfTimes,
+        display = document.querySelector(".num");
+
+
+    function noop () {
+        console.log("decoded", ref - numberOfTimes);
+        return --numberOfTimes && next(), (display.innerHTML = ref - numberOfTimes);
+    }
+    function onload () {
+        context.decodeAudioData(this.response, noop, onerror);
+        this.response = null;
+    }
+    function onerror () {
+        numberOfTimes = 0;
+        display.innerHTML = "ERROR";
+        console.log("error loading/decoding data");
+    }
+    function next () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", fileName, true);
+        xhr.responseType = "arraybuffer";
+        xhr.addEventListener("load", onload, false);
+        xhr.addEventListener("error", onerror, false);
+        xhr.send(null);
+    }
+    next();
+})();
+
